7d54827745f57be01663052dffd4d078f768e41b
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-07-08 09:29:29 -0400

    fancybox updates

--- js/app.js
+++ js/app.js
@@ -1,35 +1,37 @@
-$(function(){
-	$("#video-search").submit(function(event) {
-		event.preventDefault();
-		var userInput = $("#troll").val();
-		getResults(userInput);
-		$("#troll").val("");
-		$("#thumbnails ul").empty();
-	});
+$(function() {
+    $("#video-search").submit(function(event) {
+        event.preventDefault();
+        var userInput = $("#troll").val();
+        getResults(userInput);
+        $("#troll").val("");
+        $("#thumbnails ul").empty();
+    });
+
+    $(".fancybox").fancybox();
 });
 
 function getResults(userInput) {
-	var params = {
-		part: 'snippet',
-		key: 'AIzaSyBDDTlpeiQKDwX45GvGOg9GQ9v2ynzXugw',
-		q: userInput,
-		type: 'video'
-	};
-	url = 'https://www.googleapis.com/youtube/v3/search';
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyBDDTlpeiQKDwX45GvGOg9GQ9v2ynzXugw',
+        q: userInput,
+        type: 'video'
+    };
+    url = 'https://www.googleapis.com/youtube/v3/search';
 
-	$.getJSON(url, params, function(data){
-		showResults(data);
-	});
+    $.getJSON(url, params, function(data) {
+        showResults(data.items);
+    });
 };
 
 function showResults(results) {
-	var fillEmptyDiv = "";
+    var fillEmptyDiv = "";
     $.each(results, function(index, value) {
         console.log(value);
         fillEmptyDiv += "<li>"
-        fillEmptyDiv += "<p>" + /*snippet-->title*/ + "</p>";
-        fillEmptyDiv += "<a href='https://www.youtube.com/watch?v=" + /*id-->videoId*/ + "' target='_blank'>";
-        fillEmptyDiv += "<img src='" + /*snippet-->thumbnails-->medium-->url*/ + "'/>";
+        fillEmptyDiv += "<p>" + value.snippet.title + "</p>";
+        fillEmptyDiv += "<a class='fancybox.iframe' rel='group' href='http://www.youtube.com/embed/" + value.id.videoId + "'>";
+        fillEmptyDiv += "<img src='" + value.snippet.thumbnails.medium.url + "'/>";
         fillEmptyDiv += "</a>"
         fillEmptyDiv += "</li>"
     });
